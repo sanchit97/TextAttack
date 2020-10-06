c6fca9a6f919d54fe5ca6f324553e7daf5c6f9c6
Jin Yong Yoo <jy2ma@virginia.edu>
2020-10-06 16:42:21 -0400

    add consistent is_black_box attributes

--- textattack/search_methods/beam_search.py
+++ textattack/search_methods/beam_search.py
@@ -45,7 +45,7 @@ class BeamSearch(SearchMethod):
         return best_result
 
     @property
-    def is_blackbox(self):
+    def is_black_box(self):
         return True
 
     def extra_repr_keys(self):
